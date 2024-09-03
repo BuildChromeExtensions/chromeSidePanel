42200e0138e4098fe9d54768d7ed20c70689bb36
M2K Developments <m2ksoftiez@gmail.com>
2024-09-03 14:40:20 +0200

    Side Bar Extension

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # chromeSidePanel
 A chrome extensions that shows how side panels work in chrome extension development.
+
+<img src="./screenshot.png" width="500" alt="Screenshot" />
---
+++ background.js
@@ -0,0 +1,9 @@
+
+// When Action Icon is clicked
+chrome.action.onClicked.addListener((tab) => {
+    
+    // Open Side Panel
+    chrome.sidePanel.open({ tabId: tab.id }, () => {
+        console.log("Side Panel Opened");
+    });
+});
